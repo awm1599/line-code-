dcf785c195438fffd75d95176cde8ba8d3ad1e25
awm1599 <43990873+awm1599@users.noreply.github.com>
2019-07-13 22:03:23 +0800

    Update day_3.py

--- day_3.py
+++ day_3.py
@@ -1,21 +1,30 @@
 #Step 1: Data Preprocessing
 
 #Importing the libraries
+
 import pandas as pd
 import numpy as np
-Importing the dataset
+
+#Importing the dataset
+
 dataset = pd.read_csv('50_Startups.csv')
 X = dataset.iloc[ : , :-1].values
 Y = dataset.iloc[ : ,  4 ].values
-Encoding Categorical data
+
+#Encoding Categorical data
+
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder = LabelEncoder()
 X[: , 3] = labelencoder.fit_transform(X[ : , 3])
 onehotencoder = OneHotEncoder(categorical_features = [3])
 X = onehotencoder.fit_transform(X).toarray()
-Avoiding Dummy Variable Trap
+
+#Avoiding Dummy Variable Trap
+
 X = X[: , 1:]
-Splitting the dataset into the Training set and Test set
+
+#Splitting the dataset into the Training set and Test set
+
 from sklearn.cross_validation import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
 
